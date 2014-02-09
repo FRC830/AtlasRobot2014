402d083ea13d941aacc5ce007d765d4fc8fe0b95
ratpack <scottemorton@comcast.net>
2014-02-09 17:48:09 -0500

    added rangefinder class

---
+++ 2014robot/Rangefinder.cpp
@@ -0,0 +1,23 @@
+#include "Rangefinder.h"
+#include <cmath>
+
+Rangefinder::Rangefinder(Ultrasonic * us_left, Ultrasonic * us_right){
+	left = us_left;
+	right = us_right;
+	left->SetAutomaticMode(true);
+	right->SetAutomaticMode(true);
+}
+
+//angle in rads, relative to a position facing directly towards a surface
+//positive angle = clockwise from vertical
+float Rangefinder::robot_angle(){
+	float diff = left->GetRangeInches() - right->GetRangeInches();
+	float angle_rads = atan(diff / SENSOR_DISTANCE);
+	return angle_rads; 
+}
+
+//return distance between robot and nearest surface
+float Rangefinder::robot_distance(){
+	//average both distances to find distance from center
+	return (left->GetRangeInches() + right->GetRangeInches()) / 2;
+}
